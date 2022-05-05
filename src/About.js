3ce847f3bce825e5faeef85442f0b2e08d022525
Lucas <lucaspaulchan@gmail.com>
2022-05-04 23:31:18 -0700

    initial commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>React Blog About</p>
+    </main>
+  );
+};
+
+export default About;
