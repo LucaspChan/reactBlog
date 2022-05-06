a5f74f6ecaaf977a812bf42e62f469a76ac707d6
Lucas <lucaspaulchan@gmail.com>
2022-05-05 20:39:20 -0700

    organized structure

--- src/App.js
+++ src/App.js
@@ -2,97 +2,28 @@ import Layout from "./Layout";
 import Home from "./Home";
 import NewPost from "./NewPost";
 import PostPage from "./PostPage";
+import EditPost from "./EditPost";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { format } from "date-fns";
-import useWindowSize from "./hooks/useWindowSize";
-import useAxiosFetch from "./hooks/useAxiosFetch";
+import { Route, Routes } from "react-router-dom";
+import { DataProvider } from "./context/DataContext";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [search, setSearch] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-  const [postTitle, setPostTitle] = useState("");
-  const [postBody, setPostBody] = useState("");
-  const navigate = useNavigate();
-
-  const { width } = useWindowSize();
-
-  const { data, fetchError, isLoading } = useAxiosFetch(
-    "http://localhost:3500/posts"
-  );
-
-  useEffect(() => {
-    setPosts(data);
-  }, [data]);
-
-  useEffect(() => {
-    const filteredResults = posts.filter(
-      (post) =>
-        post.body.toLowerCase().includes(search.toLowerCase()) ||
-        post.title.toLowerCase().includes(search.toLowerCase())
-    );
-
-    setSearchResults(filteredResults.reverse());
-  }, [posts, search]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-    const datetime = format(new Date(), "MMMM dd, yyyy pp");
-    const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle("");
-    setPostBody("");
-    navigate("/");
-  };
-
-  const handleDelete = (id) => {
-    const postsList = posts.filter((post) => post.id !== id);
-    setPosts(postsList);
-    navigate("/");
-  };
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Layout search={search} setSearch={setSearch} width={width} />}
-      >
-        <Route
-          index
-          element={
-            <Home
-              posts={searchResults}
-              fetchError={fetchError}
-              isLoading={isLoading}
-            />
-          }
-        />
-        <Route path="post">
-          <Route
-            index
-            element={
-              <NewPost
-                handleSubmit={handleSubmit}
-                postTitle={postTitle}
-                setPostTitle={setPostTitle}
-                postBody={postBody}
-                setPostBody={setPostBody}
-              />
-            }
-          />
-          <Route
-            path=":id"
-            element={<PostPage posts={posts} handleDelete={handleDelete} />}
-          />
+    <DataProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="post">
+            <Route index element={<NewPost />} />
+            <Route path="edit/:id" element={<EditPost />} />
+            <Route path=":id" element={<PostPage />} />
+          </Route>
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Missing />} />
         </Route>
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<Missing />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </DataProvider>
   );
 }
 
